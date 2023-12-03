b077b93d967620579152da3ae4c9827b42910d74
slojanko <jankec72@gmail.com>
2023-12-03 21:09:56 +0100

    Day 2 solution

--- advent-of-code-2023/Code/Day2.cs
+++ advent-of-code-2023/Code/Day2.cs
@@ -1,16 +1,65 @@
 ﻿internal class Day2 : Solver
 {
-    class Game
+    public class Game
     {
-        int ID, red, green, blue;
+        public int ID, red, green, blue;
     }
 
     public override void SolveEasy()
     {
+        string[] input = File.ReadAllLines(".\\Inputs\\day2.txt");
+        int result = 0;
 
+        foreach (string line in input)
+        {
+            Game game = ParseGame(line);
+            if (game.red <= 12 && game.green <= 13 && game.blue <= 14)
+            {
+                result += game.ID;
+            }
+        }
+
+        PrintEasy(result);
     }
 
     public override void SolveHard()
     {
+        string[] input = File.ReadAllLines(".\\Inputs\\day2.txt");
+        int result = 0;
+
+        foreach (string line in input)
+        {
+            Game game = ParseGame(line);
+            result += game.red * game.green * game.blue;
+        }
+
+        PrintHard(result);
+    }
+
+    public Game ParseGame(string line)
+    {
+        Game game = new Game();
+
+        string[] line_split = line.Split(": ");
+        game.ID = int.Parse(line_split[0].Split(' ')[1]);
+
+        foreach(var set in line_split[1].Split("; "))
+        {
+            foreach(var cubes in set.Split(", "))
+            {
+                if (cubes.EndsWith("red"))
+                {
+                    game.red = Math.Max(game.red, int.Parse(cubes.Split(' ')[0]));
+                } else if (cubes.EndsWith("green"))
+                {
+                    game.green = Math.Max(game.green, int.Parse(cubes.Split(' ')[0]));
+                } else if (cubes.EndsWith("blue"))
+                {
+                    game.blue = Math.Max(game.blue, int.Parse(cubes.Split(' ')[0]));
+                }
+            }
+        }
+
+        return game;
     }
 }
--- advent-of-code-2023/Program.cs
+++ advent-of-code-2023/Program.cs
@@ -2,8 +2,12 @@
 {
     public static void Main(string[] args)
     {
-        Solver day1 = new Day1();
-        day1.SolveEasy();
-        day1.SolveHard();
+        //Solver day1 = new Day1();
+        //day1.SolveEasy();
+        //day1.SolveHard();
+
+        Solver day2 = new Day2();
+        day2.SolveEasy();
+        day2.SolveHard();
     }
 }
